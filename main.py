cff56b4e5ad60ff05833099f5f8e50fc7fd441a3
VishwaYS-108 <118991823+VishwaYS-108@users.noreply.github.com>
2026-04-17 12:01:34 +0530

    Initialize PyQt6 application with UI dialog

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-hello
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import uic
+import pandas as pd
+from dialog import Ui_Dialog
+
+
+
+class AppDemo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)  
+
+if __name__ == '__main__':
+    app =   QApplication([])
+    demo = AppDemo()
+    demo.show()
+    app.exec()
